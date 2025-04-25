44532c30e6f4a7dc7b9a408ae3e35940b4ec529b
User <dennismwema8@gmail.com>
2025-04-25 19:07:07 +0300

    done with abot section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,28 +1,31 @@
 import "./about.css";
+import AboutImage from '../../assets/about.jpg'
 
 const About = () => {
     return (
         <>
         <div className="about__section">
             <div className="about__section__one">
-                <h4>About Us</h4>
+                <h4><span>.</span>About Us</h4>
                 <div className="about__section__one__details">
-                    <img src="" alt="" />
+                    <img src={AboutImage} alt="" />
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed suscipit pariatur, aut quos ratione culpa animi modi ipsam! Deserunt, vitae!</p>
                 </div>
             </div>
+
+
             <div className="about__section__two">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi officia qui inventore? Fuga, voluptatum! Quas, fugiat, maiores, recusandae modi rem quos ipsam iste rerum tenetur debitis eius aliquam dolore iure.</p>
+                <p className="about__p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi officia qui inventore? Fuga, voluptatum! Quas, fugiat, maiores, recusandae modi rem quos ipsam iste rerum tenetur debitis eius aliquam dolore iure.</p>
                 <div className="about__section__two__statistics">
                     <div className="stat_one">
                         <h4>3+</h4>
                         <p>Years of progress</p>
                     </div>
-                    <div className="stat_two">
+                    <div className="stat_one">
                         <h4>10+</h4>
                         <p>Client Projects</p>
                     </div>
-                    <div className="stat_three">
+                    <div className="stat_one">
                         <h4>100%</h4>
                         <p>Satisfaction Rate</p>
                     </div>
